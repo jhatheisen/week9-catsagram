7c8cd771900d6795065e1e858890f301b12b3db4
Vian <vian.khachatourian@gmail.com>
2022-11-22 13:16:32 -0800

    day1 complete

--- public/index.js
+++ public/index.js
@@ -1,1 +1,25 @@
 // Your code here
+async function getCat() {
+    try {
+        const header = document.createElement("h1")
+        header.innerText = "Kitten Pic"
+        document.body.appendChild(header)
+        const res = await fetch("https://api.thecatapi.com/v1/images/search")
+        const data = await res.json()
+
+        console.log(data[0].url)
+        const url = data[0].url
+
+        let catImage = document.createElement("img")
+        catImage.setAttribute("src", url)
+        document.body.appendChild(catImage)
+
+
+   }
+   catch (e) {
+    console.log("no kitten pic")
+   }
+}
+
+
+window.onload = () => { getCat() }
