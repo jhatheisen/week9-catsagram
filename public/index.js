2580f47043d4f62dee6e611edc16b0e1eac6d3b8
Jhass Theisen <jhasstheisen1@gmail.com>
2022-11-23 12:32:08 -0800

    comments

--- public/index.js
+++ public/index.js
@@ -20,6 +20,63 @@ async function getCat() {
     console.log("no kitten pic")
    }
 }
+// p1
+
+
+// in html
+// create button el
+// set id attribute
+
+// windows .add event
+// save button location as var
+
+// on click
+// call change cat
+
+// prevent default
+// new func change cat
+// fetches new cat img
+// changes src of img to new cat
+
+// p2
+
+// html
+// add 2 buttons, up , down
+// add text default 0
+
+// on click get text,
+// new func
+// change to num,
+// inc text to -- or ++
+// change text on html
+
+// go back to change cat, if new cat button clicked, reset upvotes to 0
+
+// p3
+
+// in html
+// add an add comment button, and text field
+// create ul for all comments
+
+// on click call add comment
+
+// new func, add comment
+// get text field val
+// create new li
+// change li val to text field
+// add to ul
+
+// bonus delete
+
+// go back add button for each li
+// add id for each li
+
+// on click call delete comment(id of comment)
+
+// new func, delet comment
+// go in ul and delete li element
+// based on id
+
 
 
 window.onload = () => { getCat() }
